af70dbfafcea684149f14ae91d1c01f96c57a679
bhyago <bhyago@gmail.com>
2020-04-17 16:26:27 -0300

    Create JTW

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -30,8 +32,14 @@ class AuthenticateUserService {
 
     // Usuario autencicado
 
+    const token = sign({}, 'fb027b4936595f7027881fbb6747d259', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
