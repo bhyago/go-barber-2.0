7b93579bbbff4506b768fa34e8712c8530a1f6ae
bhyago <bhyago@gmail.com>
2020-04-24 19:24:56 -0300

    cria tooltip de erros no input

--- frontend/src/components/Input/styles.ts
+++ frontend/src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -23,6 +26,14 @@ export const Container = styled.div<ContainerProps>`
       margin-top: 8px;
     }
 
+
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
   ${(props) =>
     props.isFocused &&
     css`
@@ -35,6 +46,8 @@ export const Container = styled.div<ContainerProps>`
     css`
       color: #ff9000;
     `}
+
+
 
   input {
     flex: 1;
@@ -50,5 +63,21 @@ export const Container = styled.div<ContainerProps>`
   svg  {
     margin-right: 16px;
   }
-
   `;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+  span {
+    background: #c53030;
+    color:#fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`;
