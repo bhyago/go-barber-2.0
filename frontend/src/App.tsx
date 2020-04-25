4dc1ea5ff0f0956e755aa25d3d5f41e289d8bdc1
bhyago <bhyago@gmail.com>
2020-04-25 18:55:25 -0300

    Cria estrutura de toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,8 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SingUp';
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
 
 
 const App: React.FC = () => (
@@ -12,6 +13,9 @@ const App: React.FC = () => (
       <AuthProvider>
         <SignIn />
       </AuthProvider>
+
+      <ToastContainer />
+
       <GlobalStyle />
     </>
   );
--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErros';
 
 import logoImg from '../../assets/logo.svg';
@@ -44,9 +44,14 @@ const SignIn: React.FC = () => {
         password: data.password,
       });
     } catch (err) {
-      const errors = getValidationErrors(err);
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
 
-      formRef.current?.setErrors(errors);
+        formRef.current?.setErrors(errors);
+      }
+      // disparar um toast
+
+
     }
   }, [signIn]);
 
