3ec4b612236a60e3c7b92c769882ed45b1b19360
bhyago <bhyago@gmail.com>
2020-04-24 21:26:50 -0300

    lidando com erros

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -13,23 +13,19 @@ const upload = multer(uploadConfig);
 // ROTA: receber a requisição, chamar outro arquivo, devolver uma resposta.
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 usersRouter.patch(
@@ -39,19 +35,15 @@ usersRouter.patch(
   async (request, response) => {
     // console.log(request.file);
 
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const updateUserAvatar = new UpdateUserAvatarService();
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import cors from 'cors';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import colors from 'colors/safe';
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -15,6 +18,22 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log(colors.bold(colors.magenta('🚀 Server started on port 3333!')));
