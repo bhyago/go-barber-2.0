f8a8d564308781627b6ae3ddcaad305ad9e960c3
Hyago Braga <bhyago@gmail.com>
2020-04-30 13:45:46 -0300

    cria configuração de navegação

---
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Container } from './styles';
+import { View } from 'react-native';
+
+const SignUp: React.FC = () => {
+  return  <Container />;  
+};
+
+export default SignUp;
