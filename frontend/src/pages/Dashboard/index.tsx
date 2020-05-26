76fbd1b1598828f88a13195d592508697b8bcea3
bhyago <bhyago@gmail.com>
2020-05-26 13:52:30 -0300

    cria dashbord

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,5 +1,166 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>
+import { FiPower, FiClock } from 'react-icons/fi';
+
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Section,
+  Appointment,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg'
+import { useAuth } from '../../hooks/auth';
+
+
+const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+
+  const handleDataChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day) 
+    }
+  }, [])
+
+  const { signOut, user } = useAuth();
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src={user.avatar_url}
+              alt={user.name}
+            />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/40799862?s=460&u=56bad6fa65b61e0c6491f13b987c092d4797492d&v=4"
+                alt="Hyago"
+              />
+
+              <strong>Diego Fernandes</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/40799862?s=460&u=56bad6fa65b61e0c6491f13b987c092d4797492d&v=4"
+                  alt="Hyago"
+                />
+
+                <strong>Diego Fernandes</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/40799862?s=460&u=56bad6fa65b61e0c6491f13b987c092d4797492d&v=4"
+                  alt="Hyago"
+                />
+
+                <strong>Diego Fernandes</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/40799862?s=460&u=56bad6fa65b61e0c6491f13b987c092d4797492d&v=4"
+                  alt="Hyago"
+                />
+
+                <strong>Diego Fernandes</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar>
+          <DayPicker
+          weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+          fromMonth={new Date()}
+          disabledDays={[{ daysOfWeek: [0, 6]}]}
+          modifiers={{
+            available: { daysOfWeek: [1, 2, 3, 4, 5]}
+          }}
+          onDayClick= {handleDataChange}
+          selectedDays= {selectedDate}
+          months= {[
+            'Janeiro',
+            'Fevereiro',
+            'Março',
+            'Abril',
+            'Maio',
+            'Junho',
+            'Julho',
+            'Agosto',
+            'Setembro',
+            'Outubro',
+            'Novembro',
+            'Dezembro',
+          ]}
+          
+          />
+        </Calendar>
+      </Content>
+    </Container>
+  )
+}
 
 export default Dashboard;
